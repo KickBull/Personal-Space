b6ddb641019a7b3e74fae3d9f20976b4ea3ec9d0
KickBull <54203437+KickBull@users.noreply.github.com>
2019-12-06 06:56:31 +0200

    Update spacify.py

--- Functions/spacify.py
+++ Functions/spacify.py
@@ -21,7 +21,7 @@ def spacify(inp: str):
 
         if len(buffer) > 1:
             if buffer[1] in operators:
-                if buffer[1] in ("+", "-") and not final[-1].isdigit() and not final[-1] in (")"):
+                if buffer[1] in ("+", "-") and not final[-1].isdigit() and not final[-1] in (")") or buffer[1] == "^":
                     final = f"{final}{buffer[1]}"
                 else:
                     final = f"{final} {buffer[1]} "
