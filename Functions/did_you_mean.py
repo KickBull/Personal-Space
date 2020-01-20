eb5d97fa9e5c110a706d16a1d1f1781a59890db2
KickBull <54203437+KickBull@users.noreply.github.com>
2020-01-20 22:33:19 +0200

    not done

---
+++ Functions/did_you_mean.py
@@ -0,0 +1,28 @@
+
+#
+# def did_you_mean(inp, correct):
+#     count = 0
+#     c = 0
+#     for i, char in enumerate(list(inp)):
+#         c += 1
+#         print(c)
+#         if count >= len(correct):
+#             break
+#
+#         elif char == correct[i]:
+#             count += 1
+#
+#     if count == len(correct):
+#         return "no mistakes found"
+#
+#     if count >= len(correct) - 3 or count <= len(correct) + 2:
+#         return f"Did you mean '{correct}'?"
+#
+#     return "no matches found"
+#
+# # print(did_you_mean("snakas", "ananas"))
+# # print(did_you_mean("ananas", "ananas"))
+# print(did_you_mean("bruuuuuh", "hahahak"))
+
+import enchant
+print(enchant.Dict("en_US"))
--- Functions/spacify.py
+++ Functions/spacify.py
@@ -3,7 +3,7 @@
 def spacify(inp: str):
     """Mathematical equation space fixer.
     Example:
-    
+
     input: 4- (3 /2)*-16^ 12 -1*(+13)
     output: 4 - (3 / 2) * -16^12 - 1 * (+13)
     """
@@ -13,11 +13,11 @@ def spacify(inp: str):
     final = ""
 
     for char in list(inp):
-        if not len(buffer+char) >= 3:
-            buffer+=char
+        if not len(buffer + char) >= 3:
+            buffer += char
         elif len(buffer+char) >= 3:
             buffer = buffer[1]
-            buffer+=char
+            buffer += char
 
         if len(buffer) > 1:
             if buffer[1] in operators:
@@ -28,7 +28,7 @@ def spacify(inp: str):
             elif buffer[1] == " ":
                 final = final
             else:
-                final = final+buffer[1]
+                final = final + buffer[1]
         else:
             final = buffer[0]
 
