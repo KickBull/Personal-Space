3bd6fdc9b66f615c80c7d72a314b558c12b0a1dc
KickBull <54203437+KickBull@users.noreply.github.com>
2020-02-05 10:33:33 +0200

    OPTIMIZATION!!!

--- Functions/spacify.py
+++ Functions/spacify.py
@@ -13,20 +13,20 @@ def spacify(inp: str):
     final = ""
 
     for char in list(inp):
-        if not len(buffer + char) >= 3:
+        if len(buffer + char) <= 2:
             buffer += char
-        elif len(buffer + char) >= 3:
+        else:
             buffer = buffer[1]
             buffer += char
 
         if len(buffer) > 1:
             if buffer[1] in operators:
-                if buffer[1] in ("+", "-") and not final[-1].isdigit() and not final[-1] in (")") or buffer[1] == "^":
+                if buffer[1] in ("+", "-") and not final[-1].isdigit() and not final[-1] == ")" or buffer[1] == "^":
                     final = f"{final}{buffer[1]}"
                 else:
                     final = f"{final} {buffer[1]} "
             elif buffer[1] == " ":
-                final = final
+                pass
             else:
                 final += buffer[1]
         else:
